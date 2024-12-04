0a0c025fc606544fcbc43625ee21f6aa7030d5ae
Aleksei Nesmeev <siriusstudio84@gmail.com>
2024-12-04 07:14:06 +0300

    added base_page.py, conftest.py, requirements.txt

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope="function")
+def driver():
+    # driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
